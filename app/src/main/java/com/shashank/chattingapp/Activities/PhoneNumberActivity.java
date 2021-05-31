436fae2ee4a371b30f726b2053550deea9e3a3fd
Shashank Yadav <shashank.yadav3032@gmail.com>
2021-05-31 17:30:05 +0530

    First Commit

---
+++ app/src/main/java/com/shashank/chattingapp/Activities/PhoneNumberActivity.java
@@ -0,0 +1,48 @@
+package com.shashank.chattingapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.shashank.chattingapp.databinding.ActivityPhoneNumberBinding;
+
+import java.util.Objects;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null) {
+            Intent intent = new Intent(PhoneNumberActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        getSupportActionBar().hide();
+
+        binding.nameBox.requestFocus();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
+                intent.putExtra("phoneNumber", binding.nameBox.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+}
